1d57222320e09e5b5fe0342060a85dba7f0f41d7
Jason Weinzierl <weinzierljason@gmail.com>
2024-06-21 14:14:58 -0500

    feat: add new static website with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'pco-topaz': '#42A5F5',
+        'pco-emerald': '#68BB6A',
+      },
+    },
+  },
+  plugins: [],
+};
